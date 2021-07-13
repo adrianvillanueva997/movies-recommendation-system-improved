83dd09f2e1c94984b665ca560b9a01ee5ddb267b
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2021-07-13 21:36:10 +0200

    Basic movie GET endpoint

--- services/movies/src/routes/movies/movies.ts
+++ services/movies/src/routes/movies/movies.ts
@@ -5,7 +5,19 @@ const prisma = new PrismaClient();
 const router = express.Router();
 
 router.get("/movie", async (req: Request, res: Response) => {
-  const movies = await prisma.movie.findMany();
+  const movies = await prisma.movie.findMany({
+    select: {
+      id: true,
+      title: true,
+      Genres: {
+        select: {
+          id: true,
+          movieId: false,
+          Genre: true,
+        },
+      },
+    },
+  });
   res.send(movies);
 });
 
