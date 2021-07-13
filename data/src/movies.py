87a246f8d8c308fce6ddfcec3b4625225840ac42
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2021-07-13 21:10:14 +0200

    Finished movies ETL process

--- data/src/movies.py
+++ data/src/movies.py
@@ -6,32 +6,29 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import text
 from dotenv import load_dotenv
 
-
-class Movie:
-    title = Column(String, nullable=False)
-
-    def __init__(self, title) -> None:
-        self.title = title
-
-    def __repr__(self) -> str:
-        return f'Movie({self.title})'
-
-    def __str__(self) -> str:
-        return self.title
-
-
 load_dotenv()
 engine = create_engine(os.getenv("db_data"))
 df = pd.read_csv("data/movies.csv")
 print(str(df["genres"][0]).split("|"))
 with engine.connect() as conn:
-    query = text('INSERT INTO public."Movies"(title)VALUES(:_movie);')
     print("[INFO] Inserting movies.")
-    try:
-        for i in range(0, len(df["title"])):
+    query = text('INSERT INTO public."Movie" (title)VALUES(:_movie);')
+    for i in range(0, len(df["title"])):
+        try:
             movie = (df["title"][i])
+            print(f"Inserting: {movie}")
             insert = conn.execute(query, _movie=movie)
-        print("test")
+        except Exception as e:
+            print(e)
+    try:
+        query = text(
+            'INSERT INTO public."Genres"("Genre", "movieId")VALUES(:_genre, :_id);')
+        _id = 1
+        for i in range(0, len(df["genres"])):
+            genres = str(df["genres"][i]).split("|")
+            print(f"Inserting: {genres}")
+            for genre in genres:
+                conn.execute(query, _id=_id, _genre=genre)
+            _id += 1
     except Exception as e:
         print(e)
-    print("[INFO] Inserting tags")
